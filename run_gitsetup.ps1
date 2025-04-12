c3732d0467a122cbec825f158b27c57cd6d01e49
Ali <allashgari@yahoo.com>
2025-04-12 09:58:09 -0700

    first commit

---
+++ run_gitsetup.ps1
@@ -0,0 +1,7 @@
+echo "# pyqt_practice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:CoyoteCute/pyqt_practice.git
+git push -u origin main
